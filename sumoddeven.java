5c49b9a5da9d06b1ae1c65970161459822711d1b
Savio <mca9@kletech.ac.in>
2024-06-20 11:46:13 +0530

    Initial Commit

---
+++ sumoddeven.java
@@ -0,0 +1,40 @@
+public class sumofodde {
+    int odd;
+    int even;
+
+    void sumoddev(int a) {
+        odd = 0;
+        even = 0;
+
+        for (int i=0; i<=a; i++) {
+            if (a%2 == 1) {
+                odd = odd + i;
+            }
+            else {
+                even = even + i;
+            }
+        }
+
+        System.out.println("Sum of odd numbers is: ");
+        System.out.println(odd);
+        System.out.println("Sum of even numbers is: ");
+        System.out.println(even);
+
+    }
+
+    
+    public static void main (String args[]) {
+        sumofodde obj1 = new sumofodde();
+        int b;
+
+        System.out.println("Enter any number: ");
+        // b = toInt(System.out.readln());
+        b=3;
+
+
+
+        obj1.sumoddev(b);
+
+    }
+}
+
